0ac4bc6b1d7d82f0138116c1a6bd2fd6a36d67f4
Shahbaz Hassan <shahbazhassan24@gmail.com>
2022-06-17 15:35:02 +0500

    Initial commit

---
+++ src/Providers/PdfVersionConverterProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Shahbazhassan\PdfVersionConverter\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PdfVersionConverterProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
